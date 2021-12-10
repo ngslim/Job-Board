3f51328b505b78a45cb4bded2d62b1ff649d3d03
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2021-12-10 13:20:54 +0700

    them bootstrap scss va handlebars

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
-//Template engine
-app.engine('handlebars', handlebars());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './resources/views'));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/news', (req, res) => res.render('news'));
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
