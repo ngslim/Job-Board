b2c241afc7c6d4bac0e48534ce18e93ebcda8e38
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2021-12-29 18:37:45 +0700

    Revert "thêm trang thái của đơn ứng tuyển, thêm chấp thuận hoặc từ chối ứng tuyển"
    This reverts commit d99fd1e867d674c6d2a59011fe47fb04e281a82f.

--- src/app/models/Apply.js
+++ src/app/models/Apply.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const Apply = new Schema({
   job_id: { type: String, default: '' },
   job_owner: { type: String, default: '' },
-  status: { type: String, default: 'Đang duyệt' },
   username: { type: String, default: '' },
   created: { type: Date, default: Date.now },
 });
