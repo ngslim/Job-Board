e9386bf10ea965ff0f370eaf0e3e3644ff9d1862
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2021-12-29 15:15:39 +0700

    thêm lỗi chưa có công việc và chưa có apply, sửa tên lỗi 404

--- src/routers/site.js
+++ src/routers/site.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.use('/error', siteController.error);
+router.use('/404', siteController.error_404);
+router.use('/no-job', siteController.error_no_job);
+router.use('/no-apply', siteController.error_no_apply);
 router.use('/test', siteController.test);
 router.use('/', siteController.index);
 
