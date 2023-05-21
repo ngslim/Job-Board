30021102b5e9f286dc4f8673ee4e629ceca83e1e
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-05-21 12:56:20 +0700

    Revert "clone project"
    This reverts commit ced3f42e892702b240001af729732b2dbf674e1f.

--- src/config/constant/index.js
+++
@@ -1,5 +0,0 @@
-const constant = {
-  API_KEY: "AIzaSyC6kqnMFsIL9bf3i3aveZGHXT-ULDv18_8",
-};
-
-export default constant;
--- src/mock/user-pet/pets-options.js
+++
@@ -1,5 +0,0 @@
-export const petOptions = [
-  { id: 1, value: "Grange Reddell", label: "Grange Reddell" },
-  { id: 2, value: "Roz Gilgryst", label: "Roz Gilgryst" },
-  { id: 3, value: "Cherye Hamby", label: "Cherye Hamby" },
-];
